5719cd850367ccee002ff2b81881fcfb13bfa0ce
QUANGMIREA <117605155+QUANGMIREA@users.noreply.github.com>
2025-12-20 00:40:08 +0300

    Update conftest.py

--- menu_service/tests/conftest.py
+++ menu_service/tests/conftest.py
@@ -1,18 +1,16 @@
 import pytest
 from fastapi.testclient import TestClient
+from menu_service.app.main import app
+from menu_service.app.endpoints import menu_router
 
-from app.main import app
-from app.endpoints import menu_router
-
+@pytest.fixture(autouse=True)
+def clear_db():
+    menu_router.menu_db.clear()
+    menu_router.cart_db.clear()
+    yield
+    menu_router.menu_db.clear()
+    menu_router.cart_db.clear()
 
 @pytest.fixture
 def client():
-    # Dùng context manager để đảm bảo startup/shutdown chạy đúng
-    with TestClient(app) as c:
-        # QUAN TRỌNG: clear SAU khi startup chạy
-        menu_router.menu_db.clear()
-        menu_router.cart_db.clear()
-        yield c
-        # clear sau test luôn cho chắc
-        menu_router.menu_db.clear()
-        menu_router.cart_db.clear()
+    return TestClient(app)
