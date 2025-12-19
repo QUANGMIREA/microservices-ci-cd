f734ff318e2d35a710ebe99239978e7973019f27
QUANGMIREA <117605155+QUANGMIREA@users.noreply.github.com>
2025-12-19 23:35:29 +0300

    Update menu_router.py

--- menu_service/app/endpoints/menu_router.py
+++ menu_service/app/endpoints/menu_router.py
@@ -1,16 +1,23 @@
 from fastapi import APIRouter
 from app.models.menu_item import MenuItem
+import uuid
 
 router = APIRouter(prefix="/menu", tags=["Menu"])
 
-menu_db: list[MenuItem] = []
-cart_db: list[MenuItem] = []
+menu_db: list[dict] = []
+cart_db: list[dict] = []
 
 
 @router.post("/")
 def add_menu_item(item: MenuItem):
-    menu_db.append(item)
-    return {"message": "Item added successfully", "item": item}
+    item_dict = item.dict()
+
+    # đảm bảo có id
+    if not item_dict.get("id"):
+        item_dict["id"] = str(uuid.uuid4())
+
+    menu_db.append(item_dict)
+    return {"message": "Item added successfully", "item": item_dict}
 
 
 @router.get("/")
@@ -20,9 +27,9 @@ def get_menu():
 
 @router.post("/choose_item/{item_id}")
 def choose_item(item_id: str):
-    item = next((i for i in menu_db if i.id == item_id), None)
+    item = next((i for i in menu_db if i["id"] == item_id), None)
     if item is None:
-        return {"message": "Item not found."}
+        return {"message": "Item not found"}
 
     cart_db.append(item)
     return {"message": "Item has been added to cart", "item": item}
@@ -35,5 +42,5 @@ def view_cart():
 
 @router.get("/drinks/")
 def get_drinks():
-    drinks = [item for item in menu_db if item.type == "drink"]
+    drinks = [i for i in menu_db if i["type"].lower() == "drink"]
     return {"drinks": drinks}
