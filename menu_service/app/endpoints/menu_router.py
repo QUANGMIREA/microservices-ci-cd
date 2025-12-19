d17a7958d0c9ab38c5d2a754c38db936e3bf4d30
QUANGMIREA <117605155+QUANGMIREA@users.noreply.github.com>
2025-12-19 23:15:33 +0300

    Update menu_router.py

--- menu_service/app/endpoints/menu_router.py
+++ menu_service/app/endpoints/menu_router.py
@@ -3,36 +3,38 @@ from menu_service.app.models.menu_item import MenuItem
 
 router = APIRouter(prefix="/menu", tags=["Menu"])
 
-menu_db = []  
+menu_db = []
 cart_db = []
 
 
 @router.post("/")
 def add_menu_item(item: MenuItem):
-    menu_db.append(item)  
-    return {"message": "Item added successfully", "item": item}
+    item_dict = item.model_dump()   # 🔥 QUAN TRỌNG
+    menu_db.append(item_dict)
+    return {"message": "Item added successfully", "item": item_dict}
+
 
 @router.get("/")
 def get_menu():
     return menu_db
 
+
 @router.post("/choose_item/{item_id}")
 def choose_item(item_id: str):
-    # Find menu items by ID
-    item = next((i for i in menu_db if i.id == item_id), None)
+    item = next((i for i in menu_db if i["id"] == item_id), None)
     if item is None:
         return {"message": "Item not found."}
 
-    # Add item to cart
     cart_db.append(item)
     return {"message": "Item has been added to cart", "item": item}
 
+
 @router.get("/cart/")
 def view_cart():
-    # Show all items in cart
     return {"cart": cart_db}
+
 
 @router.get("/drinks/")
 def get_drinks():
-    drinks = [item for item in menu_db if item.type.lower() == "drink"]
+    drinks = [item for item in menu_db if item.get("type") == "drink"]
     return {"drinks": drinks}
